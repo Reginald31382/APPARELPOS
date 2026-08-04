d6326669e31df64d3dfb23146eacb177000d9e03
Reginald J Dixon <vassarjonez@yahoo.com>
2026-08-04 17:33:48 -0400

    debug

--- client/src/services/socketService.js
+++ client/src/services/socketService.js
@@ -1,8 +1,9 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000", {
+const socket = io(import.meta.env.VITE_SOCKET_URL, {
   transports: ["websocket"],
   autoConnect: false,
+  withCredentials: true,
 });
 
 export default socket;
